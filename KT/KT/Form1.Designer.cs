3f26dbb434f303c8c95c9ff7429dff4b2c4b8ab4
PhongWindair <tranphong120560@gmail.com>
2024-12-19 15:10:09 +0700

    Add files via upload

---
+++ KT/KT/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace KT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtten = new System.Windows.Forms.TextBox();
+            this.txtms = new System.Windows.Forms.TextBox();
+            this.del = new System.Windows.Forms.Button();
+            this.edit = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cblophoc = new System.Windows.Forms.ComboBox();
+            this.dateNgaySinh = new System.Windows.Forms.DateTimePicker();
+            this.exit = new System.Windows.Forms.Button();
+            this.find = new System.Windows.Forms.Button();
+            this.txtfind = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 16);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Ngay sinh";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(351, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 16);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Lop hoc";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(360, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 16);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Ho ten";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 16);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Ma sinh vien";
+            // 
+            // txtten
+            // 
+            this.txtten.Location = new System.Drawing.Point(412, 37);
+            this.txtten.Name = "txtten";
+            this.txtten.Size = new System.Drawing.Size(190, 22);
+            this.txtten.TabIndex = 13;
+            // 
+            // txtms
+            // 
+            this.txtms.Location = new System.Drawing.Point(115, 37);
+            this.txtms.Name = "txtms";
+            this.txtms.Size = new System.Drawing.Size(190, 22);
+            this.txtms.TabIndex = 14;
+            // 
+            // del
+            // 
+            this.del.Location = new System.Drawing.Point(550, 248);
+            this.del.Name = "del";
+            this.del.Size = new System.Drawing.Size(85, 36);
+            this.del.TabIndex = 9;
+            this.del.Text = "xoa";
+            this.del.UseVisualStyleBackColor = true;
+            this.del.Click += new System.EventHandler(this.del_Click);
+            // 
+            // edit
+            // 
+            this.edit.Location = new System.Drawing.Point(440, 249);
+            this.edit.Name = "edit";
+            this.edit.Size = new System.Drawing.Size(85, 36);
+            this.edit.TabIndex = 10;
+            this.edit.Text = "sua";
+            this.edit.UseVisualStyleBackColor = true;
+            this.edit.Click += new System.EventHandler(this.edit_Click);
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(330, 249);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(85, 36);
+            this.add.TabIndex = 11;
+            this.add.Text = "them";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(39, 305);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(950, 249);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // cblophoc
+            // 
+            this.cblophoc.FormattingEnabled = true;
+            this.cblophoc.Location = new System.Drawing.Point(412, 88);
+            this.cblophoc.Name = "cblophoc";
+            this.cblophoc.Size = new System.Drawing.Size(190, 24);
+            this.cblophoc.TabIndex = 15;
+            // 
+            // dateNgaySinh
+            // 
+            this.dateNgaySinh.Location = new System.Drawing.Point(115, 90);
+            this.dateNgaySinh.Name = "dateNgaySinh";
+            this.dateNgaySinh.Size = new System.Drawing.Size(200, 22);
+            this.dateNgaySinh.TabIndex = 20;
+            // 
+            // exit
+            // 
+            this.exit.Location = new System.Drawing.Point(880, 255);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(108, 23);
+            this.exit.TabIndex = 23;
+            this.exit.Text = "tro ve";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // find
+            // 
+            this.find.Location = new System.Drawing.Point(197, 249);
+            this.find.Name = "find";
+            this.find.Size = new System.Drawing.Size(108, 32);
+            this.find.TabIndex = 22;
+            this.find.Text = "tim kiem";
+            this.find.UseVisualStyleBackColor = true;
+            this.find.Click += new System.EventHandler(this.find_Click);
+            // 
+            // txtfind
+            // 
+            this.txtfind.Location = new System.Drawing.Point(39, 256);
+            this.txtfind.Name = "txtfind";
+            this.txtfind.Size = new System.Drawing.Size(152, 22);
+            this.txtfind.TabIndex = 21;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(660, 249);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(85, 36);
+            this.btnSave.TabIndex = 10;
+            this.btnSave.Text = "luu";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(770, 247);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(85, 36);
+            this.btnCancel.TabIndex = 9;
+            this.btnCancel.Text = "bo qua";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dateNgaySinh);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cblophoc);
+            this.groupBox1.Controls.Add(this.txtten);
+            this.groupBox1.Controls.Add(this.txtms);
+            this.groupBox1.Location = new System.Drawing.Point(210, 53);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(644, 145);
+            this.groupBox1.TabIndex = 24;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thong tin sinh vien";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1021, 566);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.find);
+            this.Controls.Add(this.txtfind);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.del);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.edit);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtten;
+        private System.Windows.Forms.TextBox txtms;
+        private System.Windows.Forms.Button del;
+        private System.Windows.Forms.Button edit;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox cblophoc;
+        private System.Windows.Forms.DateTimePicker dateNgaySinh;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.Button find;
+        private System.Windows.Forms.TextBox txtfind;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ KT/KT/Form1.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KT
+{
+    public partial class Form1 : Form
+    {
+        private bool changesPending = false; 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                var context = new Model1();
+                var listsinhvien = context.Sinhviens
+                    .Select(s => new
+                    {
+                        s.MaSV,
+                        s.HotenSV,
+                        s.Ngaysinh,
+                        Tenlop = s.Lop.Tenlop,
+                    }).ToList();
+
+                dataGridView1.DataSource = listsinhvien;
+                var listlop = context.Lops.ToList();
+                Fillkhoa(listlop);
+
+                dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+                btnSave.Enabled = false;
+                btnCancel.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void Fillkhoa(List<Lop> listlop)
+        {
+            cblophoc.DataSource = listlop;
+            cblophoc.DisplayMember = "Tenlop";
+            cblophoc.ValueMember = "Malop";
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var context = new Model1())
+                {
+                    var sinhvien = new Sinhvien
+                    {
+                        MaSV = txtms.Text,
+                        HotenSV = txtten.Text,
+                        Ngaysinh = dateNgaySinh.Value,
+                        Malop = cblophoc.SelectedValue.ToString()
+                    };
+
+                    context.Sinhviens.Add(sinhvien);
+                    context.SaveChanges();
+
+                    MessageBox.Show("Thêm sinh viên thành công!");
+                    LoadData();
+                    ClearInputFields();
+
+                    ShowSaveCancelButtons();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi: {ex.Message}");
+            }
+        }
+        private void LoadData()
+        {
+            try
+            {
+                using (var context = new Model1())
+                {
+                    var listsinhvien = context.Sinhviens
+                        .Select(s => new
+                        {
+                            s.MaSV,
+                            s.HotenSV,
+                            s.Ngaysinh,
+                            Tenlop = s.Lop.Tenlop,
+                        }).ToList();
+
+                    dataGridView1.DataSource = listsinhvien;
+
+                    btnSave.Enabled = false;
+                    btnCancel.Enabled = false;
+                    changesPending = false; 
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void ClearInputFields()
+        {
+            txtms.Text = string.Empty;
+            txtten.Text = string.Empty;
+            dateNgaySinh.Value = DateTime.Now;
+            cblophoc.SelectedIndex = -1;
+        }
+        private void ShowSaveCancelButtons()
+        {
+            btnSave.Enabled = true;
+            btnCancel.Enabled = true;
+            changesPending = true; 
+        }
+
+        private void edit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dataGridView1.CurrentRow != null)
+                {
+                    string maSV = dataGridView1.CurrentRow.Cells["MaSV"].Value.ToString();
+
+                    using (var context = new Model1())
+                    {
+                        var sinhvien = context.Sinhviens.FirstOrDefault(s => s.MaSV == maSV);
+
+                        if (sinhvien != null)
+                        {
+                            sinhvien.HotenSV = txtten.Text;
+                            sinhvien.Ngaysinh = dateNgaySinh.Value;
+                            sinhvien.Malop = cblophoc.SelectedValue.ToString();
+
+                            context.SaveChanges();
+
+                            MessageBox.Show("Cập nhật thông tin sinh viên thành công!");
+                            LoadData();
+                            ClearInputFields();
+
+                            ShowSaveCancelButtons();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Không tìm thấy sinh viên để sửa.");
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Vui lòng chọn một sinh viên để sửa.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi: {ex.Message}");
+            }
+        }
+
+        private void del_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dataGridView1.CurrentRow != null)
+                {
+                    var result = MessageBox.Show("Bạn có chắc chắn muốn xóa sinh viên này không?",
+                                                 "Xác nhận xóa",
+                                                 MessageBoxButtons.YesNo,
+                                                 MessageBoxIcon.Question);
+
+                    if (result == DialogResult.Yes)
+                    {
+                        string maSV = dataGridView1.CurrentRow.Cells["MaSV"].Value.ToString();
+
+                        using (var context = new Model1())
+                        {
+                            var sinhvien = context.Sinhviens.FirstOrDefault(s => s.MaSV == maSV);
+
+                            if (sinhvien != null)
+                            {
+                                context.Sinhviens.Remove(sinhvien);
+                                context.SaveChanges();
+
+                                MessageBox.Show("Xóa sinh viên thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                LoadData();
+                                ClearInputFields();
+
+                                ShowSaveCancelButtons(); 
+                            }
+                            else
+                            {
+                                MessageBox.Show("Không tìm thấy sinh viên để xóa.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Vui lòng chọn một sinh viên để xóa.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi: {ex.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Tất cả thay đổi đã được lưu.");
+            btnSave.Enabled = false;
+            btnCancel.Enabled = false;
+            changesPending = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LoadData();
+            MessageBox.Show("Các thay đổi đã bị hủy.");
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show("Bạn có chắc chắn muốn thoát không?",
+                             "Xác nhận thoát",
+                             MessageBoxButtons.YesNo,
+                             MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void find_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var context = new Model1())
+                {
+                    if (!string.IsNullOrWhiteSpace(txtfind.Text))
+                    {
+                        var searchName = txtfind.Text.ToLower();
+
+                        var result = context.Sinhviens
+                            .Where(s => s.HotenSV.ToLower().Contains(searchName))
+                            .Select(s => new
+                            {
+                                s.MaSV,
+                                s.HotenSV,
+                                s.Ngaysinh,
+                                Tenlop = s.Lop.Tenlop
+                            })
+                            .ToList();
+
+                        dataGridView1.DataSource = result;
+
+                        if (result.Count == 0)
+                        {
+                            MessageBox.Show("Không tìm thấy sinh viên phù hợp.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Vui lòng nhập tên sinh viên để tìm kiếm.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi: {ex.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                if (e.RowIndex >= 0) 
+                {
+                    DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+
+                    txtms.Text = row.Cells["MaSV"].Value?.ToString();
+                    txtten.Text = row.Cells["HotenSV"].Value?.ToString();
+                    dateNgaySinh.Value = row.Cells["Ngaysinh"].Value != null
+                        ? Convert.ToDateTime(row.Cells["Ngaysinh"].Value)
+                        : DateTime.Now;
+                    cblophoc.Text = row.Cells["Tenlop"].Value?.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi: {ex.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
